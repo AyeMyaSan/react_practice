e5df4982f77b7820c479897db0882f730c2ba336
Aye Mya San <104755767+AyeMyaSan@users.noreply.github.com>
2022-10-21 15:39:44 +0630

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,13 +4,13 @@ import React from 'react';
 const Home = ({history}) => {
     return (
         <div>
-            <h2>홈</h2>
+            <h2>Home</h2>
             <button
                 onClick={() => {
-                    //자바스크립트로 경로 이동을 할 때.
+                
                     history.push('/about/javascript')
                 }}>
-            자바스크립트를 사용하여 이동
+         Javascript
             </button>
         </div>
     );
